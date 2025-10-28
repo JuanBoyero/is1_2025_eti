d9da69918d2a4bff8f68ca4d6254f3c89f41075c
Juan Boyero <@juaniboyerooo@gmail.com>
2025-10-28 18:15:25 -0300

    Creacion de la tabla professor (Como herencia professor --> user)

--- target/classes/scheme.sql
+++ target/classes/scheme.sql
@@ -7,3 +7,21 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+-- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
+DROP TABLE IF EXISTS professor;
+
+-- Crea la tabla 'users' con los campos originales, adaptados para SQLite
+CREATE TABLE professor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
+    name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
+    password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+    nombre TEXT NOT NULL,
+    apellido TEXT NOT NULL, 
+    correo TEXT NOT NULL,
+    dni TEXT NOT NULL,
+
+    CONSTRAINT fk_id_professor FOREIGN KEY (id) REFERENCES users(id),
+    CONSTRAINT fk_name_professor FOREIGN KEY (name) REFERENCES users(name),
+    CONSTRAINT fk_password_professor FOREIGN KEY (password) REFERENCES users(password)
+    );
