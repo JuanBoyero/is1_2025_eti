b57e092264f7a71b8e3067725b4117d036aeadef
Juan Boyero <@juaniboyerooo@gmail.com>
2025-11-01 10:39:49 -0300

    Agrego el modelo Professor y su respentiva tabla en la BD

---
+++ src/main/java/com/is1/proyecto/models/Professor.java
@@ -0,0 +1,56 @@
+package com.is1.proyecto.models;
+
+import org.javalite.activejdbc.Model;
+import org.javalite.activejdbc.annotations.Table;
+
+@Table("professor") // Esta anotación asocia explícitamente el modelo 'Professor' con la tabla 'Professor' en la DB.
+public class Professor extends Model {
+
+    // ActiveJDBC mapea automáticamente las columnas de la tabla 'users'
+    // (como 'id', 'name', 'password', etc.) a los atributos de esta clase.
+    // No necesitas declarar los campos (id, name, password) aquí como variables de instancia,
+    // ya que la clase Model base se encarga de la interacción con la base de datos.
+
+    // Opcional: Puedes agregar métodos getters y setters si prefieres un acceso más tipado,
+    // aunque los métodos genéricos de Model (getString(), set(), getInteger(), etc.) ya funcionan.
+
+    public Integer getID(){
+        return getInteger("id"); //Obtiene el valor de la columna id
+    }
+
+    public String getName() {
+        return getString("name"); // Obtiene el valor de la columna 'name'
+    }
+
+    public void setName(String name) {
+        set("name", name); // Establece el valor para la columna 'name'
+    }
+
+    public String getPassword() {
+        return getString("password"); // Obtiene el valor de la columna 'password'
+    }
+
+    public void setPassword(String password) {
+        set("password", password); // Establece el valor para la columna 'password'
+    }
+
+    public String getRealName(){
+        return getString("real_name");
+    }
+
+    public void setRealName(String realName){
+        set("real_name",realName);
+    }
+
+    public String getSurname(){
+        return getString("surname");
+    }
+
+    public void setSurname(String surname){
+        set("surname",surname);
+    }
+
+    public String getDni(){
+        return getString("dni");
+    }
+}
