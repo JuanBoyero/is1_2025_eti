28f982c0afafca194ce0678c570f658b531b6a58
Juan Boyero <@juaniboyerooo@gmail.com>
2025-11-01 10:40:52 -0300

    Cargo todos los cambios

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -7,3 +7,16 @@ CREATE TABLE users (
     name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
+
+-- Elimina la tabla 'professor' si ya existe para asegurar un inicio limpio
+DROP TABLE IF EXISTS professor;
+
+-- Crea la tabla 'users' con los campos originales, adaptados para SQLite
+CREATE TABLE professor (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,-- Clave primaria autoincremental para SQLite
+    name TEXT NOT NULL UNIQUE,           -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
+    password TEXT NOT NULL,              -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+    real_name TEXT NOT NULL,             -- Nombre del profesor     
+    surname TEXT NOT NULL,               -- Apellido del profesor
+    dni VARCHAR(8) NOT NULL,             -- DNI del profesor 
+);
--- target/classes/scheme.sql
+++ target/classes/scheme.sql
@@ -8,20 +8,15 @@ CREATE TABLE users (
     password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
 );
 
--- Elimina la tabla 'users' si ya existe para asegurar un inicio limpio
+-- Elimina la tabla 'professor' si ya existe para asegurar un inicio limpio
 DROP TABLE IF EXISTS professor;
 
 -- Crea la tabla 'users' con los campos originales, adaptados para SQLite
 CREATE TABLE professor (
-    id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
-    name TEXT NOT NULL UNIQUE,          -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
-    password TEXT NOT NULL           -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
-    nombre TEXT NOT NULL,
-    apellido TEXT NOT NULL, 
-    correo TEXT NOT NULL,
-    dni TEXT NOT NULL,
-
-    CONSTRAINT fk_id_professor FOREIGN KEY (id) REFERENCES users(id),
-    CONSTRAINT fk_name_professor FOREIGN KEY (name) REFERENCES users(name),
-    CONSTRAINT fk_password_professor FOREIGN KEY (password) REFERENCES users(password)
-    );
+    id INTEGER PRIMARY KEY AUTOINCREMENT,-- Clave primaria autoincremental para SQLite
+    name TEXT NOT NULL UNIQUE,           -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
+    password TEXT NOT NULL,              -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
+    real_name TEXT NOT NULL,             -- Nombre del profesor     
+    surname TEXT NOT NULL,               -- Apellido del profesor
+    dni VARCHAR(8) NOT NULL,             -- DNI del profesor 
+);
