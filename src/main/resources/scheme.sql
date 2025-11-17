4ff48174ac5482e5e7903f8906dac64d85159fae
Juan Boyero <@juaniboyerooo@gmail.com>
2025-11-17 20:21:02 -0300

    Reestructuracion de la tabla professor

--- src/main/resources/scheme.sql
+++ src/main/resources/scheme.sql
@@ -14,9 +14,8 @@ DROP TABLE IF EXISTS professor;
 -- Crea la tabla 'users' con los campos originales, adaptados para SQLite
 CREATE TABLE professor (
     id INTEGER PRIMARY KEY AUTOINCREMENT, -- Clave primaria autoincremental para SQLite
-    name TEXT NOT NULL UNIQUE, -- Nombre de usuario (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
-    password TEXT NOT NULL, -- Contraseña hasheada (TEXT es el tipo de cadena recomendado para SQLite)
-    real_name TEXT NOT NULL, -- Nombre del profesor     
+    email TEXT NOT NULL UNIQUE, -- email de profesor (TEXT es el tipo de cadena recomendado para SQLite), con restricción UNIQUE
+    name TEXT NOT NULL, -- Nombre del profesor     
     surname TEXT NOT NULL, -- Apellido del profesor
     dni VARCHAR(8) NOT NULL UNIQUE -- DNI del profesor 
 );
